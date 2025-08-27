ffb7fc3ea983865c36aeb3da17f91305773f1015
Mustafa Aydemir <mustafa@aydemir.im>
2025-08-27 15:21:13 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://brantial.airscarp.workers.dev',
+    integrations: [mdx(), sitemap(), react()],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    adapter: cloudflare(),
+});
---
+++ src/content.config.ts
@@ -0,0 +1,34 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    date: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val))
+      .optional(),
+    authors: z
+      .array(
+        z.object({
+          name: z.string(),
+          image: z.string().optional(),
+        }),
+      )
+      .optional(),
+    tags: z.array(z.string()).optional(),
+    coverImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
